28344ab7a08759df1d3aed6a5a8746593205d0df
María Reveles <revelesmaria4@gmail.com>
2022-09-26 10:52:09 -0500

    feat:e agrego script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+console.log("Saoko papi Saoko...");
+document.getElemetById("generation").style.color = "grey";
+
+function colorTitulo() {
+  console.log("Pulso el botón de generation");
+  document.getElementById("generation").style.color = "blue";
+}
+
+</script>
